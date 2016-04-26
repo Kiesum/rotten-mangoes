f01d9c52542062bd69227cccaeac40f4e9078c4b
Kiesum <kieran_summerfeldt@hotmail.com>
2016-04-26 18:47:28 +0000

    added login functionality with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   get 'users/new'
 
   get 'users/create'
@@ -7,6 +11,8 @@ Rails.application.routes.draw do
   resources :movies
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
