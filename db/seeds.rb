a3b243a6209428d679114728c529d6f1ba5a9f73
Kiesum <kieran_summerfeldt@hotmail.com>
2016-04-29 00:02:01 +0000

    made further changes to the homepage as well as to edit page and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'omdbapi'
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
@@ -11,17 +11,17 @@ require 'omdbapi'
 
  @movies = Imdb::Top250.new
  @movies = @movies.movies
+ 
 
- # runtime = @result.runtime.split(' ')[0].to_i
 @movies.each do |m| 
    Movie.create(
-    title: m.title,
+    title: m.title.split(".\n ")[1],
     director: m.director[0],
     runtime_in_minutes: m.length, 
     description: m.plot,
     poster_image_url: m.poster,
     release_date: DateTime.parse(m.release_date)
     )
-   puts m.title
+   puts m.title.split(".\n ")[1]
 end
 
